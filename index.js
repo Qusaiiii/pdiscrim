928c96b96f65f0f2a52bc9f6369a2444838cb209
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-19 18:47:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 const Discord = require('discord.js')
 const client = new Discord.Client();
-const prefix=  '!';
+const prefix = '!';
 
 
 
-client.on('message',async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === 'dm') return;
-  let args = message.content.split(' ');
-  let tag;
-  if(args[0] === `${prefix}discrim`) {
-    if(args[1]) {
-      let discrim = Array.from(args[1]);
-      if(isNaN(args[1])) return message.channel.send(`- \`${message.author.username}\`, يجب ان تتكون هذه الخانة من ارقام وليس احرف`);
-      if(discrim.length !== 4) return message.channel.send(`- \`${message.author.username}\`, يجب ان يكون التاق مكون من 4 ارقام`);
-
-      tag = discrim.map(r => r.toString()).join('');
-      console.log(tag);
-      if(client.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let iLD = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('Peery');
-      message.channel.send(iLD);
-    } else if(!args[1]) {
-      tag = message.author.discriminator;
-      if(client.users.filter(f => f.discriminator === tag).size === 0) return message.channel.send(`- \`${message.author.username}\`, لا يوجد احد بهذا التاق`);
-      let L4U = new Discord.RichEmbed()
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setDescription(client.users.filter(f => f.discriminator === tag).map(r => r.username).slice(0, 10).join('\n'))
-      .setFooter('Peery');
-      message.channel.send(L4U);
+client.on('message' , message => {
+  var prefix = '!';
+if(message.content === `${prefix}discrim`) {
+                      let array = [];
+                      var i = 0;
+client.users.filter(u => u.discriminator == message.author.discriminator).map(u => {
+    if(i > 4){
+     return;
     }
-  }
+    i = i + 1;
+   array.push(`${u.tag}`);
 });
+hastebin(`${array.slice(0, 30).join('\n')}`, 'txt').then(l => {
+    message.channel.send(`${l}`);
+}).catch(console.error);
 
+        }
+});
 
 
 client.on('message', message => {
